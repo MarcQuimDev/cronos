555599a46bc9484aaa475cd52702d3ce9e741618
quimturon <quimturonpi@outlook.com>
2025-12-19 10:27:51 +0100

    Main Versio

--- src/main.cpp
+++ src/main.cpp
@@ -100,7 +100,7 @@ void reconnect() {
 }
 
 //OTA
-#define FW_VERSION 1.1
+#define FW_VERSION 1.0
 bool otaInProgress = false;
 
 const char* versionURL = "https://raw.githubusercontent.com/MarcQuimDev/cronos/esp32/version.txt";
